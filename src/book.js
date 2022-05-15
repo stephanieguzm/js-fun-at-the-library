443d8487578dd43c503bf2327ccb8703abb0fc23
Stephanie Guzman <sguzman247@gmail.com>
2022-05-15 11:17:23 -0500

    Update book.js and shelf.js tests

--- src/book.js
+++ src/book.js
@@ -11,19 +11,34 @@ function buildMainCharacter(name, age, pronouns) {
 };
 
 function saveReview(criticReview, reviews) {
-  //only unique reviews are allowed to return
   if (reviews.includes(criticReview)) {
     return reviews
   }
     reviews.push(criticReview)
+    console.log(reviews)
+    return reviews
 }
+// -----------
+//     !reviews.includes(criticReview)
+//     reviews.push(criticReview);
+//   //if the reviews array does not include the criticReview
+//   // !reviews.includes(criticReview)
+//     console.log(reviews);
+//     return reviews
+//   }
+//   ---------
+//   //then push the criticReview into the reviews array
+//     reviews.push(criticReview)
+//     console.log(reviews)
+//     return reviews
+// }
 
 function calculatePageCount(createTitle) {
     return createTitle.length * 20;
   }
 
 function writeBook(bookTitle, bookCharacter, genre) {
-  var numOfPages = bookTitle.length * 20;
+  numOfPages = bookTitle.length * 20;
   return {
     title: bookTitle,
     mainCharacter: bookCharacter,
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,33 @@
+function shelfBook(bookies, sciFiShelf) {
+  if (sciFiShelf.length < 3) {
+  sciFiShelf.unshift(bookies)
+}
+  // console.log(shelf)
+  return shelf
+}
+
+function unshelfBook(bookies, sciFiShelf) {
+  // for loop runs through every element in the sciFiShelf array
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    // bookies strictly equals to the specific "string" in the title key
+    if (sciFiShelf[i].title === bookies) {
+      // we want to splice at the same position where i matches value to be removed
+      return sciFiShelf.splice(i, 1)
+    }
+  }
+}
+
+function listTitles(titles, bookNames) {
+
+
+
+}
+// return the titles "Hyperion, Dune, Ender's Game"
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+    shelfBook,
+    unshelfBook,
+    listTitles,
   // searchShelf
 };
